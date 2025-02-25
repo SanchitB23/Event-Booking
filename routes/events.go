5238f6b5e29a3d1118f4c46bd4a3b3f915b0c2c6
sanchitb23 <dev.sanchitb23@gmail.com>
2025-02-26 02:25:24 +0530

    Implemented Auth verification in update and delete

--- routes/events.go
+++ routes/events.go
@@ -55,10 +55,15 @@ func updateEvent(context *gin.Context) {
 		return
 	}
 
-	_, err = models.GetEventByID(eventId)
+	event, err := models.GetEventByID(eventId)
 
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "message": "Failed to retrieve event"})
+		return
+	}
+
+	if event.UserID != context.GetInt64("userId") {
+		context.JSON(http.StatusForbidden, gin.H{"message": "You are not authorized to update this event"})
 		return
 	}
 
@@ -91,6 +96,12 @@ func deleteEvent(context *gin.Context) {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "message": "Failed to retrieve event"})
 		return
 	}
+
+	if event.UserID != context.GetInt64("userId") {
+		context.JSON(http.StatusForbidden, gin.H{"message": "You are not authorized to update this event"})
+		return
+	}
+
 	err = event.Delete()
 	if err != nil {
 		context.JSON(http.StatusInternalServerError, gin.H{"error": err.Error(), "message": "Failed to delete event"})
